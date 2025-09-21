fa960711d97c963371b7fd9b720c76a3db175879
Si Thu San <sithusanss@gmail.com>
2025-09-21 16:50:14 +0700

    feat: readline

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,164 @@
 package request
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+const CLRF = "\r\n"
+
+const VERSION_PART_SIZE = 2
+
+var requestLineParts = map[string]int{
+	"method":        0,
+	"requestTarget": 1,
+	"httpVersion":   2,
+}
+
+// empty struct allocate zero memory
+var supportedMethods = map[string]struct{}{
+	"GET":    {},
+	"POST":   {},
+	"PUT":    {},
+	"PATCH":  {},
+	"DELETE": {},
+	"HEAD":   {},
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	request, err := io.ReadAll(reader)
+
+	if err != nil {
+		return nil, err
+	}
+
+	requestLine, err := parseRequestLine(request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{
+		RequestLine: *requestLine,
+	}, nil
+}
+
+func parseRequestLine(request []byte) (*RequestLine, error) {
+
+	idx, err := checkCLRF(request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	parts, err := getRequestLineParts(request, idx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	method, err := getMethod(parts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	requestTarget, err := getRequestTarget(parts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	httpVersion, err := getHttpVersion(parts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &RequestLine{
+		HttpVersion:   httpVersion,
+		RequestTarget: requestTarget,
+		Method:        method,
+	}, nil
+}
+
+func checkCLRF(request []byte) (int, error) {
+
+	idx := bytes.Index(request, []byte(CLRF))
+
+	if idx == -1 {
+		return 0, fmt.Errorf("could not find CRLF in request-line")
+	}
+
+	return idx, nil
+}
+
+func getRequestLineParts(request []byte, idx int) ([]string, error) {
+
+	requestLine := string(request[:idx])
+
+	parts := strings.Split(requestLine, " ")
+
+	if len(parts) != len(requestLineParts) {
+		return nil, fmt.Errorf("not enough parts")
+	}
+
+	return parts, nil
+}
+
+func getMethod(parts []string) (string, error) {
+	method := parts[requestLineParts["method"]]
+
+	for _, char := range method {
+		if char < 'A' || char > 'Z' {
+			return "", fmt.Errorf("method needs be in capital")
+		}
+	}
+
+	if _, ok := supportedMethods[method]; !ok {
+		return "", fmt.Errorf("unsupported method")
+	}
+
+	return method, nil
+}
+
+func getRequestTarget(parts []string) (string, error) {
+	requestTarget := parts[requestLineParts["requestTarget"]]
+
+	if requestTarget == "" {
+		return "", fmt.Errorf("malformed request target")
+	}
+
+	return requestTarget, nil
+}
+
+func getHttpVersion(parts []string) (string, error) {
+	versionParts := strings.Split(parts[requestLineParts["httpVersion"]], "/")
+
+	if len(versionParts) != VERSION_PART_SIZE {
+		return "", fmt.Errorf("malformed http version")
+	}
+
+	if versionParts[0] != "HTTP" {
+		return "", fmt.Errorf("unrecognized http version")
+	}
+
+	// Currently, ONLY SUPPORT 1.1.
+	if versionParts[1] != "1.1" {
+		return "", fmt.Errorf("unrecognized http version")
+	}
+
+	return versionParts[1], nil
+}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,59 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
+	// Test: Good GET Request line
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good GET Request line with path
+	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good POST Request line with path
+	r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Invalid number of parts in request line
+	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Invalid method(small case) in request line
+	_, err = RequestFromReader(strings.NewReader("get /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Invalid method(unknow method) in request line
+	_, err = RequestFromReader(strings.NewReader("UNKNOWN /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Invalid method(out of order) in request line
+	_, err = RequestFromReader(strings.NewReader("/coffee GET HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Invalid httpVersion(different version) in request line
+	_, err = RequestFromReader(strings.NewReader("Get /coffee HTTP/3.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Invalid httpVersion(unknown protocol) in request line
+	_, err = RequestFromReader(strings.NewReader("Get /coffee UNKNOWN/3.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
 }
