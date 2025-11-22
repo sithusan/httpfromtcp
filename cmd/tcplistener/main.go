5ceff85b522e2286207fe65c70ed2653c3ea77c9
Si Thu San <sithusanss@gmail.com>
2025-11-22 15:14:53 +0700

    feat: live headers

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -41,6 +41,11 @@ func main() {
 		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
 		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 
+		fmt.Println("Headers:")
+		for key, value := range r.Headers {
+			fmt.Printf("- %s: %s\n", key, value)
+		}
+
 		fmt.Println("Connection is closed from", conn.RemoteAddr())
 	}
 }
